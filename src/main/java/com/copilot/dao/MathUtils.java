48601160fa52dab287427221463f3ffc2af0583a
Mukesh kumar <kumar.mukesh@impressico.com>
2023-03-03 18:56:52 +0530

    add isPrime

--- src/main/java/com/copilot/dao/MathUtils.java
+++ src/main/java/com/copilot/dao/MathUtils.java
@@ -18,5 +18,18 @@ public class MathUtils {
             return a / b;
         }
 
-        
+        //is prime?
+        public boolean isPrime(int a) {
+            if (a <= 1) {
+                return false;
+            }
+            for (int i = 2; i < a; i++) {
+                if (a % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
 }
